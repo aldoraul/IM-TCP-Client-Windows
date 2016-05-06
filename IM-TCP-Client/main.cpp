21eb1b7bc3d86e231a6d3f0c3848bbe5faa47172
Aldo Anaya <ara34c@mail.umkc>
2016-05-06 11:40:04 -0500

    able to send and recieve (on seperate socket) info to and from server

--- IM-TCP-Client/main.cpp
+++ IM-TCP-Client/main.cpp
@@ -10,6 +10,7 @@
 	It will need to encrypt outgoing, and decrypt incoming messages*/
 
 #include<stdio.h>
+//#include<Windows.h>
 #include<winsock2.h>
 #include<ws2tcpip.h>
 #include<process.h>
@@ -19,14 +20,18 @@
 #include<iostream>
 #include"cipher.h"
 
-using namespace std;
 
+using namespace std;
+int end_flag = 1;	// set to 0 to end program
+int flag1 = 0; // 0 pass, 1 stop to read
+#define	BACKLOG 10
+#define BUFSIZE 500
 char *server = "192.168.10.200";
 char *port = "34567";
 int typeOfMessage = 0;
 char server_reply[1000];
 void listen(void *);		// listen to socket continuesly with second thread
-void *get_in_addr(struct sockaddr *);	// used to see whether remote addr is IPv4 or IPv6
+int New_Process(SOCKET);
 void sendMsg(int);
 string encryptMessage(string);
 struct active_user {
@@ -38,7 +43,7 @@ struct active_user {
 
 };
 list<active_user> users;
-SOCKET s;
+SOCKET s_notUsed;
 string user = "";
 int main() {
 	WSADATA wsa;
@@ -47,9 +52,7 @@ int main() {
 	users.push_back(server1);
 	//	new 4.26.16-00:18
 	struct sockaddr_in server;
-	//struct sockaddr_in server;	
-	//char *message;
-//	int recv_size;
+
 	int return_value;
 	cout << "Welcome to CS423 TCP IM program" << endl;
 
@@ -57,7 +60,13 @@ int main() {
 		printf("Failed. Error Code : %d", WSAGetLastError());		
 		return 1;
 	}
-	struct addrinfo server_struct, *servinfo,*p;
+	printf("Enter your IM name: \n");
+	getline(cin, user);
+
+	// use send msg function to send initial function.  not created yet 11:31:4/24/16
+	_beginthread(listen, 0, (void *)&s_notUsed);  // create listen function for second thread
+	sendMsg(1);
+/*	struct addrinfo server_struct, *servinfo,*p;
 //	struct sockaddr_storage their_addr;
 	memset(&server_struct, 0, sizeof server_struct);
 	server_struct.ai_family = AF_UNSPEC;
@@ -71,16 +80,6 @@ int main() {
 		return 1;
 	}
 	s = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);	
-	//set SO_RESESADDR on a socket s to true	
-	// new 4.26.16-00:16
-	//if ((s = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
-	//	printf("Could not create socket: %d", WSAGetLastError());
-	//	return 1;
-	//}
-	//server.sin_addr.s_addr = inet_addr(SERVER);
-	//server.sin_family = AF_INET;
-	//server.sin_port = htons(34567);
-
 
 	int optval = 1;
 	setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (const char*)&optval, sizeof optval);
@@ -88,23 +87,9 @@ int main() {
 	//Connect to remote/local TCP server	
 	connect(s, servinfo->ai_addr, servinfo->ai_addrlen);
 	printf("Connected to %s\n", server1.addr);
-	//char *message;
-	//message = "hello world";
-	//if (send(s, message, strlen(message), 0) < 0) {
-	//	cout << "Send Failed" << endl;
-	//	return 1;
-	//}
-	//else {
-	//	cout << "sent" << endl;
-	//}
-
+	*/
 	//				NEW
-	printf("Enter your IM name: \n");
-	getline(cin, user);
 
-	// use send msg function to send initial function.  not created yet 11:31:4/24/16
-	sendMsg(1);
-	_beginthread(listen, 0, (void *)&s);  // create listen function for second thread
 	char action = 'c';
 
 	while (action != 'q') {
@@ -128,15 +113,16 @@ int main() {
 				if (typeOfMessage == 0)
 					printf("No new Messages\n");
 				else {
-					printf("New Message Arrived \n");
+					printf("\n\nNew Message Arrived \n");
 					cout << server_reply << endl;
-					typeOfMessage == 0;
+					typeOfMessage = 0;
 				}
 				break;
 			}
 			case 'Q':
 			case 'q':
 			{
+				end_flag = 0;
 				action = 'q';
 				break;
 			}
@@ -145,7 +131,7 @@ int main() {
 		}
 
 	}
-
+	Sleep(500);
 /*
 	//			original code  
 	//Send some data									//
@@ -171,57 +157,103 @@ int main() {
 
 }
 
-void listen(void * socket) {
-	int recv_size;
-	int last_char = 0;
-	SOCKET s;
-	string num;
-	s = *(SOCKET *)socket;
-	char new_reply[500];
+void listen(void * sock) {
+	int recv_size, rv;
+	SOCKET s1, newfd;
+	char *listenPort = "34567";
+	struct addrinfo hints, *servinfo1, *p1;
+	int yes = 1;
+
+	memset(&hints, 0, sizeof hints);
+	printf("thread 1 yes\n");
+
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	//hints.ai_flags = AI_PASSIVE;
+
+	if ((rv = getaddrinfo("192.168.10.101", listenPort, &hints, &servinfo1)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		exit(1);
+	}
+
+	for (p1 = servinfo1; p1 != NULL; p1 = p1->ai_next) {
+		if ((s1 = socket(p1->ai_family, p1->ai_socktype, p1->ai_protocol)) == -1) {
+			perror("TCP_Server: socket");
+			continue;
+		}
+		if (setsockopt(s1, SOL_SOCKET, SO_REUSEADDR, (const char*)&yes, sizeof(int)) == -1) {
+			perror("setsockopt");
+			exit(1);
+		}
+		if (bind(s1, p1->ai_addr, p1->ai_addrlen) == -1) {
+			closesocket(s1);
+			perror("TCP_Server: bind");
+			continue;
+		}
+		break;
+	}
+	if (servinfo1 == NULL) {
+		fprintf(stderr, "TCP_Server: failed to bind socket\n");
+		exit(1);
+	}
+
+	freeaddrinfo(p1);
+
+
+	if (listen(s1, BACKLOG) == -1) {
+		perror("listen");
+		exit(1);
+	}
+	struct sockaddr_storage remote_addr;
+	socklen_t addr_len;
+
 	while (true) {
-		if (typeOfMessage == 0){
-			do {
-				if ((recv_size = recv(s, new_reply, 500, 0)) != SOCKET_ERROR) {
-					for (int i = last_char; i < (last_char + recv_size); i++) {
-						server_reply[i] = new_reply[i];
-					}
-					last_char += recv_size;
-				}
-			} while (new_reply[recv_size] != '#');
-			if (isdigit(server_reply[0])) {
-				num = server_reply[0];
-				typeOfMessage = server_reply[0] - '0';
-			}
-			else {
-				typeOfMessage = 5; // for error
-			}
+		addr_len = sizeof remote_addr;
+		newfd = accept(s1, (struct sockaddr *)&remote_addr, &addr_len);
+		if (newfd == INVALID_SOCKET) {
+			printf("accept error %d\n", GetLastError());
+		}
+		if (_beginthread((void(*)(void *))New_Process, 0, (void *)newfd) < 0)
+			printf("_beginthread: %s\n", strerror(errno));
+		if (end_flag == 0) {
+			break;
 		}
 	}
-	//printf("\n\nNew Message is %s\n", server_reply);
+
+	closesocket(newfd);
 }
 
-void *get_in_addr(struct sockaddr *sa) {
-	if (sa->sa_family == AF_INET) {
-		return &(((struct sockaddr_in*)sa)->sin_addr);
-	}
-	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+int New_Process(SOCKET fd) {
+	//char	buf[BUFSIZE];
+	printf("thread 2 yes");
+	int	cc;
+	cc = recv(fd, server_reply, sizeof server_reply, 0);
+	typeOfMessage = 4;
+	closesocket(fd);
+	return 0;
 }
 
 void sendMsg(int msgType) {
 	struct addrinfo serv, *server_info;
-	//int return_value;
+	int return_value;
 	char *message;
 	string msg = "";
+	int optval = 1;
+
+	memset(&serv, 0, sizeof serv);
+	serv.ai_family = AF_UNSPEC;
+	serv.ai_socktype = SOCK_STREAM;
 
 	switch (msgType) {
 		case 1:
 		{
-			/*if ((return_value = getaddrinfo(SERVER, PORT, &serv, &server_info)) != 0) {
-				printf("getaddrinfo: %s\n", gai_strerror(return_value));
+			if ((return_value = getaddrinfo("192.168.10.200", "34567", &serv, &server_info)) != 0) {
+				printf("FFaddrinfo: %s\n", gai_strerror(return_value));
 				exit(1);//return 1;
 			}
 			SOCKET s = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
-			connect(s, server_info->ai_addr, server_info->ai_addrlen);*/
+			setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (const char*)&optval, sizeof optval);
+			connect(s, server_info->ai_addr, server_info->ai_addrlen);
 			msg = "1;" + user + ";34567#";
 			message = new char[msg.length() + 1];
 			strcpy(message, encryptMessage(msg).c_str()); // encrypt message and switch string to char* to send message
@@ -229,7 +261,7 @@ void sendMsg(int msgType) {
 				cout << "Send Failed" << endl;					//
 				exit(1);										//
 			}
-			//closesocket(s);
+			closesocket(s);
 		}
 		default:
 			break;
